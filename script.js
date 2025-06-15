fdff0de95de543cab0b135bf9d634270371ead1a
ibmohanti <azomic2000@hotmail.com>
2025-06-15 14:00:27 +0100

    Enhanced styles and interactivity

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function showMessage() {
     alert("Hello ! This website is interactive.");
 }
+
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Thank you, " + document.getElementById("name").value + "!");
+}) ;
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+document.querySelectorAll ( 'a[href^="#"]' ).forEach(anchor => {
+anchor.addEventListener("click", function(event){
+    event.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({behavior: "smooth"});
+});
+});
